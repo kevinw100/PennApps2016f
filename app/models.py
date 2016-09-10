be25cba911a8ed203f2117bde7cbde8b5890d587
Chris <christophermartin749@gmail.com>
2016-09-10 13:46:32 -0400

    models.py has candidate and user table

--- app/models.py
+++ app/models.py
@@ -1,3 +1,4 @@
+
 # from app import db
 
 # class User(db.Model):
@@ -16,4 +17,27 @@ class User(Base):
     id = Column(Integer, primary_key = True)
     first_name = Column(String(50), nullable = False)
     last_name = Column(String(50), nullable = False)
-    
+
+    def __init__(self, first_name, last_name):
+        self.first_name = first_name
+        self.last_name = last_name
+
+    def __repr__(self):
+        return '<User %r %r>' % (self.first_name, self.last_name)
+
+class Candidates(Base):
+    __tablename__ = 'candidates'
+
+    id = Column(Integer, primary_key = True)
+    name = Column(String(80), nullable = False)
+
+    def __init__(self, name):
+        self.name = name
+
+    def __repr__(self):
+        return '<Candidate %r>' % (self.name)
+
+class Votes(Base):
+    __tablename__ = 'votes'
+    user_id = Column(Integer, primary_key = True)
+    rank =
