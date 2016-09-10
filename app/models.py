43890f98786016651a6f960e8cc161a58c810c65
Kevin Wang <kevinw100@gmail.com>
2016-09-10 15:42:23 -0400

    modified models.py

--- app/models.py
+++ app/models.py
@@ -18,11 +18,12 @@ class User(Base):
     first_name = Column(String(50), nullable = False)
     last_name = Column(String(50), nullable = False)
     email = Column(String(50), nullable = False)
-
-    def __init__(self, first_name, last_name, email):
+    password = Column(String(50), nullable = False)
+    def __init__(self, first_name, last_name, email, password):
         self.first_name = first_name
         self.last_name = last_name
         self.email = email
+        self.password = password
 
     def __repr__(self):
         return '<User %r %r>' % (self.first_name, self.last_name)
